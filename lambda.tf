fd12234e9e3574b76eafb3f215f9c80fb0d3c2c5
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-05-08 00:13:04 +0900

    冗長なlocal変数名の変更 / archive_fileの利用でオレオレ実装の削除とその他不要な記述を削除して簡略化

--- lambda.tf
+++ lambda.tf
@@ -1,15 +1,15 @@
 resource "aws_lambda_function" "helloworld" {
-  function_name    = "typescript-sample-helloworld"
+  function_name = "typescript-sample-helloworld"
 
-  s3_bucket        = aws_s3_bucket.lambda_assets.bucket
-  s3_key           = data.aws_s3_object.package.key
+  s3_bucket = aws_s3_bucket.lambda_assets.bucket
+  s3_key    = aws_s3_object.package.key
 
-  source_code_hash = data.aws_s3_object.package_hash.body
+  source_code_hash = data.archive_file.lambda_package.output_base64sha256
   handler          = "index.handler"
 
-  role             = aws_iam_role.iam_for_lambda.arn
-  runtime          = "nodejs20.x"
-  timeout          = "10"
+  role    = aws_iam_role.iam_for_lambda.arn
+  runtime = "nodejs20.x"
+  timeout = "10"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
@@ -36,61 +36,33 @@ resource "aws_iam_role" "iam_for_lambda" {
 }
 
 resource "null_resource" "lambda_build" {
-  depends_on = [aws_s3_bucket.lambda_assets]
-
   triggers = {
     code_diff = join("", [
-      for file in fileset(local.helloworld_function_dir_local_path, "{*.ts, package*.json}")
-      : filebase64("${local.helloworld_function_dir_local_path}/${file}")
+      for file in fileset(local.src_path, "{*.ts, package*.json}")
+      : filebase64("${local.src_path}/${file}")
     ])
   }
 
   provisioner "local-exec" {
-    command = "cd ${local.helloworld_function_dir_local_path} && npm install"
+    command = "cd ${local.src_path} && npm install"
   }
   provisioner "local-exec" {
-    command = "cd ${local.helloworld_function_dir_local_path} && npm run build"
-  }
-  provisioner "local-exec" {
-    command = "aws s3 cp ${local.helloworld_function_package_local_path} s3://${aws_s3_bucket.lambda_assets.bucket}/${local.helloworld_function_package_s3_key}"
-  }
-  provisioner "local-exec" {
-    command = "openssl dgst -sha256 -binary ${local.helloworld_function_package_local_path} | openssl enc -base64 | tr -d \"\n\" > ${local.helloworld_function_package_base64sha256_local_path}"
-  }
-  provisioner "local-exec" {
-    command = "aws s3 cp ${local.helloworld_function_package_base64sha256_local_path} s3://${aws_s3_bucket.lambda_assets.bucket}/${local.helloworld_function_package_base64sha256_s3_key} --content-type \"text/plain\""
+    command = "cd ${local.src_path} && npm run build"
   }
 }
 
 resource "aws_s3_bucket" "lambda_assets" {}
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "lambda_assets" {
+resource "aws_s3_object" "package" {
   bucket = aws_s3_bucket.lambda_assets.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-resource "aws_s3_bucket_public_access_block" "lambda_assets" {
-  bucket = aws_s3_bucket.lambda_assets.bucket
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  key    = local.package_s3_key
+  source = data.archive_file.lambda_package.output_path
 }
 
-data "aws_s3_object" "package" {
+data "archive_file" "lambda_package" {
   depends_on = [null_resource.lambda_build]
 
-  bucket = aws_s3_bucket.lambda_assets.bucket
-  key    = local.helloworld_function_package_s3_key
-}
-data "aws_s3_object" "package_hash" {
-  depends_on = [null_resource.lambda_build]
-
-  bucket = aws_s3_bucket.lambda_assets.bucket
-  key    = local.helloworld_function_package_base64sha256_s3_key
+  type        = "zip"
+  source_dir  = local.build_path
+  output_path = local.package_path
 }
--- main.tf
+++ main.tf
@@ -7,5 +7,9 @@ terraform {
       source  = "hashicorp/null"
       version = ">= 3.2.2"
     }
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.4.2"
+    }
   }
 }
