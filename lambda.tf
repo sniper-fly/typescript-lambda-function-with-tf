69cef32302a2750377f974a421e3802cda1e6cee
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-05-08 10:37:01 +0900

    resource順番並び替え

--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,38 @@
+resource "null_resource" "lambda_build" {
+  triggers = {
+    code_diff = join("", [
+      for file in fileset(local.src_path, "{*.ts, package*.json}")
+      : filebase64("${local.src_path}/${file}")
+    ])
+  }
+  provisioner "local-exec" {
+    working_dir = local.src_path
+    command     = "npm install"
+    on_failure  = fail
+  }
+  provisioner "local-exec" {
+    working_dir = local.src_path
+    command     = "npm run build"
+    on_failure  = fail
+  }
+}
+
+data "archive_file" "lambda_package" {
+  depends_on = [null_resource.lambda_build]
+
+  type        = "zip"
+  source_dir  = local.build_path
+  output_path = local.package_path
+}
+
+resource "aws_s3_bucket" "lambda_assets" {}
+
+resource "aws_s3_object" "package" {
+  bucket = aws_s3_bucket.lambda_assets.bucket
+  key    = local.package_s3_key
+  source = data.archive_file.lambda_package.output_path
+}
+
 resource "aws_lambda_function" "helloworld" {
   function_name = "typescript-sample-helloworld"
 
@@ -13,7 +48,7 @@ resource "aws_lambda_function" "helloworld" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name = "role-for-sample-ts-lambda"
+  name               = "role-for-sample-ts-lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -30,40 +65,4 @@ data "aws_iam_policy_document" "assume_role" {
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "null_resource" "lambda_build" {
-  triggers = {
-    code_diff = join("", [
-      for file in fileset(local.src_path, "{*.ts, package*.json}")
-      : filebase64("${local.src_path}/${file}")
-    ])
-  }
-
-  provisioner "local-exec" {
-    working_dir = local.src_path
-    command     = "npm install"
-    on_failure  = fail
-  }
-  provisioner "local-exec" {
-    working_dir = local.src_path
-    command     = "npm run build"
-    on_failure  = fail
-  }
-}
-
-resource "aws_s3_bucket" "lambda_assets" {}
-
-resource "aws_s3_object" "package" {
-  bucket = aws_s3_bucket.lambda_assets.bucket
-  key    = local.package_s3_key
-  source = data.archive_file.lambda_package.output_path
-}
-
-data "archive_file" "lambda_package" {
-  depends_on = [null_resource.lambda_build]
-
-  type        = "zip"
-  source_dir  = local.build_path
-  output_path = local.package_path
 }
