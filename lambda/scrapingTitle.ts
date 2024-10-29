1b2e3d3dae393d23f8f0f925b5921bb08f3da2ad
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-10-29 10:25:44 +0900

    chromiumバイナリのpathを指定

--- lambda/scrapingTitle.ts
+++ lambda/scrapingTitle.ts
@@ -3,9 +3,14 @@ import chromium from "@sparticuz/chromium";
 import "dotenv/config";
 
 export async function scrapingTitle(url: string) {
+  const binPath =
+    process.env.NODE_ENV === "production"
+      ? "/opt/nodejs/node_modules/@sparticuz/chromium/bin"
+      : undefined;
+
   const browser = await playwright.launch({
     args: chromium.args,
-    executablePath: await chromium.executablePath(),
+    executablePath: await chromium.executablePath(binPath),
     headless: true,
   });
   const page = await browser.newPage();
