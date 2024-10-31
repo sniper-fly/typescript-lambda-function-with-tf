e48d5c057e550581e9d8fa78c4faace47b4cee9c
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-10-31 15:31:34 +0900

    パッケージビルド時にchromiumをexternalとすることでlambda layer使用時にexecutablePathが正しく動くようにする

--- lambda/scrapingTitle.ts
+++ lambda/scrapingTitle.ts
@@ -3,14 +3,9 @@ import chromium from "@sparticuz/chromium";
 import "dotenv/config";
 
 export async function scrapingTitle(url: string) {
-  const binPath =
-    process.env.NODE_ENV === "production"
-      ? "/opt/nodejs/node_modules/@sparticuz/chromium/bin"
-      : undefined;
-
   const browser = await playwright.launch({
     args: chromium.args,
-    executablePath: await chromium.executablePath(binPath),
+    executablePath: await chromium.executablePath(),
     headless: true,
   });
   const page = await browser.newPage();
@@ -18,7 +13,7 @@ export async function scrapingTitle(url: string) {
   const pageTitle = await page.title();
 
   if (process.env.NODE_ENV !== "production") {
-    await browser.close(); // lambdaだとエラーになってしまうのでコメントアウト
+    await browser.close();
   }
   return pageTitle;
 }
