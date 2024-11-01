8ab8b6ab00f2c1f202c261a93aac9d339d430369
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-11-01 12:21:23 +0900

    terraform local ファイルでのchromiumのバージョン指定を簡便にできるように

--- locals.tf
+++ locals.tf
@@ -3,6 +3,8 @@ locals {
   build_path              = "${local.src_path}/dist"
   package_path            = "${local.src_path}/index.zip"
   package_s3_key          = "index.zip"
-  chromium_zip            = "chromium-v130.0.0-layer.zip"
+
+  chromium_version        = "130.0.0"
+  chromium_zip            = "chromium-v${local.chromium_version}-layer.zip"
   chromium_layer_zip_path = "${path.module}/${local.chromium_zip}"
 }
